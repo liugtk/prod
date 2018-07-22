0762abed438fc839672bbf7a9ec34739d9dd6958
DESKTOP-O16UD8S\Carter Liu <gtk.carter.liu@gmail.com>
2018-07-22 20:41:18 +0800

    validation for the length above 2 + invlid records can not be saved

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -5,14 +5,30 @@ describe('Validating records',()=>{
     it ("requires a user name",()=>{
         const user = new User({name : undefined});
         const validationResult = user.validateSync();
-        //console.log(validationResult);
-        //const {message} = validationResult.errors.name;
-        console.log('wwwwwow: '+ message);
+        const {message} = validationResult.errors.name;
         assert (message === 'Name is required.');
         /*
         user,validate((validationResult)=>{
             // asynchronous validation
         })
          */
+    });
+
+    it ('requires a name longer than 2 characters',()=>{
+       const user = new User({name: 'Al'});
+       const validationResult = user.validateSync();
+       const {message} = validationResult.errors.name;
+       assert( message === 'Name must be longer than 2 characters.');
+    });
+
+    it ('disallows invalid records from being saved', (done) => {
+        const user = new User({name:'AL'});
+        user.save()
+            .then()
+            .catch((validationResult)=>{
+                const {message} = validationResult.errors.name;
+                assert( message === 'Name must be longer than 2 characters.');
+                done();
+        });
     })
 });
