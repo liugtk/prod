646cbb107bef55ee441ae7bbcffca8dae2e3b7ee
DESKTOP-O16UD8S\Carter Liu <gtk.carter.liu@gmail.com>
2018-07-22 10:33:24 +0800

    32 instance remove

---
+++ users/test/delete_test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+
+describe('Deleting a user', ()=>{
+    let joe;
+
+    beforeEach((done)=>{
+        joe = new User({name:'Joe'});
+        joe.save()
+            .then(()=>done());
+    });
+    // model instance => joe
+    //class => User
+    it('model instance remove',(done)=>{
+        joe.remove()
+            .then(()=>
+                User.findOne({name:'Joe'})
+            )
+            .then((user)=>{
+                assert(user === null);
+                done();
+            })
+    });
+
+    it ('class method remove', ()=>{
+        
+    });
+
+    it ('class method findAndRemove',()=>{
+
+    });
+
+    it('class method findByIdRemove',()=>{
+
+    });
+})
